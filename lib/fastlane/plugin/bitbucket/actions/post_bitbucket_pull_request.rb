d7e071ef1daeba1b532f00f6c772c16d8b40d04d
Luca Tagliabue <lu.tagliabue@reply.it>
2024-01-24 17:24:32 +0100

    Add correct options values as func parameters

--- lib/fastlane/plugin/bitbucket/actions/post_bitbucket_pull_request.rb
+++ lib/fastlane/plugin/bitbucket/actions/post_bitbucket_pull_request.rb
@@ -6,7 +6,7 @@ module Fastlane
     module SharedValues
       POST_BITBUCKET_PULL_REQUEST_RESULT = :POST_BITBUCKET_PULL_REQUEST_RESULT
     end
-    class POSTBITBUCKETPULLREQUESTACTION  < Action
+    class PostBitbucketPullRequestAction  < Action
       def self.run(options)
         begin
           require 'excon'
@@ -15,11 +15,19 @@ module Fastlane
           project_key = options[:project_key]
           repository_name = options[:repository_name]
 
+          api_token = Base64.encode64("#{options[:username]}:#{options[:password]}")
+
+          puts "DEBUG api_token"
+          puts api_token
+
           api_url = "https://bitbucket.#{company_host_name}.com/rest/api/1.0/projects/#{project_key}/repos/#{repository_name}/pull-requests"
 
-          headers = { "Content-Type": "application/json", "Authorization": "Basic #{options[:api_token]}" }
+          headers = { "Content-Type": "application/json", "Authorization": "Basic #{api_token}" }
 
-          reviewers = option[:reviewers].map { |reviewer| 
+          puts "DEBUG headers"
+          puts headers
+
+          reviewers = options[:reviewers].map { |reviewer| 
               {
                 user: {
                    name: reviewer    
@@ -27,35 +35,43 @@ module Fastlane
               }
             }
 
+          puts "DEBUG reviewers"
+          puts reviewers
+
           payload = {
             title: options[:title],
             description: options[:description],
-            state: OPEN,
+            state: "OPEN",
             open: true,
             closed: false,
             fromRef: {
-                id: options[:from_ref_id],
+                id: options[:source_branch],
                 repository: {
-                    slug: my-repo,
-                    name: null,
+                    slug: "my-repo",
+                    name: nil,
                     project: {
-                        key: PRJ
+                        key: "PRJ"
                     }
                 }
             },
             toRef: {
-                id: options[:to_ref_id],
+                id: options[:destination_branch],
                 repository: {
-                    slug: my-repo,
-                    name: null,
+                    slug: "my-repo",
+                    name: nil,
                     project: {
-                        key: PRJ
+                        key: "PRJ"
                     }
                 }
             },
             locked: false,
             reviewers: reviewers
           }.to_json
+
+          puts "DEBUG payload"
+          puts payload
+
+          puts api_url
 
           response = Excon.post(api_url, headers: headers, body: payload)
           result = self.formatted_result(response)
@@ -99,13 +115,22 @@ module Fastlane
 
       def self.available_options
         [
-          FastlaneCore::ConfigItem.new(key: :api_token,
-                                       env_name: "FL_POST_BITBUCKET_PULL_REQUEST_TOKEN",
-                                       description: "Bitbucket auth token",
+          FastlaneCore::ConfigItem.new(key: :username,
+                                       env_name: "FL_POST_BITBUCKET_PULL_REQUEST_USERNAME",
+                                       description: "Bitbucket username",
                                        sensitive: true,
                                        code_gen_sensitive: true,
                                        is_string: true,
-                                       default_value: ENV["BITBUCKET_API_TOKEN"],
+                                       default_value: ENV["BITBUCKET_USERNAME"],
+                                       default_value_dynamic: true,
+                                       optional: false),
+          FastlaneCore::ConfigItem.new(key: :password,
+                                       env_name: "FL_POST_BITBUCKET_PULL_REQUEST_PASSWORD",
+                                       description: "Bitbucket password",
+                                       sensitive: true,
+                                       code_gen_sensitive: true,
+                                       is_string: true,
+                                       default_value: ENV["BITBUCKET_PASSWORD"],
                                        default_value_dynamic: true,
                                        optional: false),
           FastlaneCore::ConfigItem.new(key: :company_host_name,
@@ -138,23 +163,44 @@ module Fastlane
           FastlaneCore::ConfigItem.new(key: :title,
                                        env_name: "FL_POST_BITBUCKET_PULL_REQUEST_TITLE",
                                        description: "Title of the pull request",
+                                       is_string: true,
                                        optional: false),
           FastlaneCore::ConfigItem.new(key: :description,
                                        env_name: "FL_POST_BITBUCKET_PULL_REQUEST_DESCRIPTION",
                                        description: "Description of the pull request",
-                                       optional: false)
+                                       is_string: true,
+                                       optional: false),
+          FastlaneCore::ConfigItem.new(key: :reviewers,
+                                       env_name: "FL_POST_BITBUCKET_PULL_REQUEST_REVIEWERS",
+                                       description: "List of reviewers for the pull request",
+                                       type: Array,
+                                       optional: false),
+          FastlaneCore::ConfigItem.new(key: :source_branch,
+                                       env_name: "FL_POST_BITBUCKET_PULL_REQUEST_SOURCE_BRANCH",
+                                       description: "Name of the source branch",
+                                       is_string: true,
+                                       optional: false),
+          FastlaneCore::ConfigItem.new(key: :destination_branch,
+                                       env_name: "FL_POST_BITBUCKET_PULL_REQUEST_DESTINATION_BRANCH",
+                                       description: "Name of the destination branch",
+                                       is_string: true,
+                                       optional: false),
         ]
       end
 
       def self.example_code
         [
           'post_bitbucket_pull_request(
-            api_token: "YOUR_API_TOKEN_HERE",
-            company_host_name: "YOUR_COMPANY_HOST_HERE",
-            project_key: "YOUR_PROJECT_KEY_HERE",
-            repository_name: "YOUR_REPOSITORY_NAME_HERE",
-            title: "PULL_REQUEST_TITLE_HERE",
-            description: "PULL_REQUEST_DESCRIPTION_HERE"
+              username: "YOUR_USERNAME_HERE",
+              password: "YOUR_PASSWORD_HERE",
+              company_host_name: "YOUR_COMPANY_HOST_HERE",
+              project_key: "YOUR_PROJECT_KEY_HERE",
+              repository_name: "YOUR_REPOSITORY_NAME_HERE",
+              title: "PULL_REQUEST_TITLE_HERE",
+              description: "PULL_REQUEST_DESCRIPTION_HERE",
+              reviewers: ["FIRST_REVIEWER", "SECOND_REVIEWER"],
+              source_branch: "YOUR_SOURCE_BRANCH_HERE",
+              destination_branch: "YOUR_DESTINATION_BRANCH_HERE"
           )'
         ]
       end
